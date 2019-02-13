09a900e7d289ceb77fbf7ed0d1f5de6f68065ebb
bernard <c00235159@itcarlow.ie>
2019-02-13 22:44:06 +0000

    Tutorial 6. Adding account view, markdown

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -7,11 +7,12 @@ from boards import views
 
 urlpatterns = [
     url(r'^$', views.BoardListView.as_view(), name='home'),
-    url(r'^$', views.home, name='home'),
+    #url(r'^$', views.home, name='home'),
     url(r'^signup/$', accounts_views.signup, name='signup'),
     url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
 
+    
     url(r'^reset/$',
         auth_views.PasswordResetView.as_view(
             template_name='password_reset.html',
@@ -33,6 +34,12 @@ urlpatterns = [
         name='password_change'),
     url(r'^settings/password/done/$', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'),
         name='password_change_done'),
+
+    url(r'^settings/account/$', accounts_views.UserUpdateView.as_view(), name='my_account'),
+    url(r'^settings/password/$', auth_views.PasswordChangeView.as_view(template_name='password_change.html'),
+    name='password_change'),
+    url(r'^settings/password/done/$', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'),
+    name='password_change_done'),
 
     url(r'^boards/(?P<pk>\d+)/$', views.TopicListView.as_view(), name='board_topics'),
     url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
